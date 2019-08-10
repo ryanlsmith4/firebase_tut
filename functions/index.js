8e0e4337a5bd8071b3533b53501064c43c4101fd
Ryan Smith <ryan.l.smith4@gmail.com>
2019-08-09 23:06:06 -0700

    [ADD] First commit finished tut

---
+++ functions/index.js
@@ -0,0 +1,18 @@
+const functions = require('firebase-functions');
+
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+
+exports.sendMessage = functions.firestore
+    .document('products/{productId}')
+    .onCreate((event) => {
+      console.log(event)
+      const docId = event.params.productId;
+
+      const name = event.data.data().name;
+
+      const productRef = admin.firestore().collection('products').doc(docId)
+
+      return productRef.update({ message: `Nice ${name}! - Love Cloud Functions`})
+     
+    });
---
+++ public/app.js
@@ -0,0 +1,62 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+
+  const app = firebase.app()
+  console.log("_________________________");
+  console.log(app);
+  const db = firebase.firestore();
+
+  const myPost = db.collection('Posts').doc('firstPost');
+  const productsRef = db.collection('products');
+
+  // const query = productsRef.where('price', '>', 10)
+
+  // query.get()
+  //       .then((products) => {
+  //         products.forEach((doc) => {
+  //           data = doc.data()
+  //           document.write(`${data.name} at ${data.price} <br>`)
+  //         })
+  //       })
+
+  myPost.onSnapshot((doc) => {
+            const data = doc.data();
+            document.querySelector('#title').innerHTML = data.title
+
+          })
+
+});
+
+
+  function googleLogin() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    firebase.auth().signInWithPopup(provider)
+
+      .then((result) => {
+        const user = result.user;
+        document.write(`Hello ${user.displayName}`);
+        console.log(user);
+      })
+        .catch(console.log)
+  }
+
+  function updatePost(e) {
+    const db = firebase.firestore();
+    const myPost = db.collection('Posts').doc('firstPost');
+    myPost.update({ title: e.target.value })
+  }
+
+  function uploadFile(files) {
+    const storageRef= firebase.storage().ref();
+    const horseRef = storageRef.child('horse.jpg');
+
+    const file = files.item(0);
+
+    const task = horseRef.put(file)
+
+    task.then((snapshot) => {
+      console.log(snapshot)
+      const url = snapshot.downloadURL
+      document.querySelector('#imgUpload').setAttribute('src', url)
+    })
+  }
